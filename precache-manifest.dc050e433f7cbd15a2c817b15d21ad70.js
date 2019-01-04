9a5a751e0bea1ccee355cdafc440e19280972e2c
mingyue liu <mliu2@ltu.edu>
2019-01-03 22:28:42 -0500

    Updates

---
+++ precache-manifest.dc050e433f7cbd15a2c817b15d21ad70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0271efad476b86cd91c35c3826d8bdf4",
+    "url": "/Jianshu_React/static/media/logo.0271efad.png"
+  },
+  {
+    "revision": "711353c85e6b7040a773",
+    "url": "/Jianshu_React/static/js/runtime~main.711353c8.js"
+  },
+  {
+    "revision": "7ae835c3fd2360f1357c",
+    "url": "/Jianshu_React/static/js/main.7ae835c3.chunk.js"
+  },
+  {
+    "revision": "c317b8de16a6bf8d5ee4",
+    "url": "/Jianshu_React/static/js/1.c317b8de.chunk.js"
+  },
+  {
+    "revision": "c6d20dabf985e0dd2fae6a8427470f57",
+    "url": "/Jianshu_React/index.html"
+  }
+];
